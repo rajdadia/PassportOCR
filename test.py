616db82f45865733031a79e8e0e21a2b087ef43f
Raj Dadia <rajdadia@gmail.com>
2019-06-17 17:34:33 +0530

    changed structure

--- test.py
+++ test.py
@@ -12,17 +12,17 @@ from tqdm import tqdm
 import time
 
 
-# construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-t", "--template", required=True, help="Path to template image")
-ap.add_argument("-i", "--images", required=True,
-    help="Path to images where template will be matched")
-ap.add_argument("-v", "--visualize",
-    help="Flag indicating whether or not to visualize each iteration")
-args = vars(ap.parse_args())
+# # construct the argument parser and parse the arguments
+# ap = argparse.ArgumentParser()
+# ap.add_argument("-t", "--template", required=True, help="Path to template image")
+# ap.add_argument("-i", "--images", required=True,
+#     help="Path to images where template will be matched")
+# ap.add_argument("-v", "--visualize",
+#     help="Flag indicating whether or not to visualize each iteration")
+# args = vars(ap.parse_args())
 
 # load the image image, convert it to grayscale, and detect edges
-template = cv2.imread('./input/'+args["template"])
+template = cv2.imread('./input/3_1.png')
 template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
 template = cv2.Canny(template, 50, 200)
 (tH, tW) = template.shape[:2]
@@ -94,7 +94,7 @@ def imgName(imagePath):
 
 for i in extn:
     # loop over the images to find the template in
-    for imagePath in glob.glob(args["images"] + "/input/*_1."+i):
+    for imagePath in glob.glob("./input/*_1."+i):
         # load the image, convert it to grayscale, and initialize the
         # bookkeeping variable to keep track of the matched region
         image = cv2.imread(imagePath)
@@ -120,13 +120,13 @@ for i in extn:
             (_, maxVal, _, maxLoc) = cv2.minMaxLoc(result)
 
             # check to see if the iteration should be visualized
-            if args.get("visualize", False):
-                # draw a bounding box around the detected region
-                clone = np.dstack([edged, edged, edged])
-                cv2.rectangle(clone, (maxLoc[0], maxLoc[1]),
-                    (maxLoc[0] + tW, maxLoc[1] + tH), (0, 0, 255), 2)
-                cv2.imshow("Visualize", clone)
-                cv2.waitKey(0)
+            # if args.get("visualize", False):
+            #     # draw a bounding box around the detected region
+            #     clone = np.dstack([edged, edged, edged])
+            #     cv2.rectangle(clone, (maxLoc[0], maxLoc[1]),
+            #         (maxLoc[0] + tW, maxLoc[1] + tH), (0, 0, 255), 2)
+            #     cv2.imshow("Visualize", clone)
+            #     cv2.waitKey(0)
 
             # if we have found a new maximum correlation value, then update
             # the bookkeeping variable
